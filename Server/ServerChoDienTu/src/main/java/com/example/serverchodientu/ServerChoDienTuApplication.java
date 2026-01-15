db93e0826253f85ba53946423ca0e2c3df0c4cb0
Kahtan <0306231203@caothang.edu.vn>
2026-01-15 15:33:31 +0700

    feat: add client and server source code

---
+++ Server/ServerChoDienTu/src/main/java/com/example/serverchodientu/ServerChoDienTuApplication.java
@@ -0,0 +1,13 @@
+package com.example.serverchodientu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerChoDienTuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerChoDienTuApplication.class, args);
+    }
+
+}
---
+++ Server/ServerChoDienTu/src/test/java/com/example/serverchodientu/ServerChoDienTuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverchodientu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerChoDienTuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
